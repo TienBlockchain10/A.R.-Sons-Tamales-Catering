770025e05fd58f2430736586cb623a60e8ff6723
TienBlockchain10 <nguyensecure@proton.me>
2024-01-24 11:07:07 -0600

    changed cart to be shown on mobile and for it to be a modal pop up

--- js/custom.js
+++ js/custom.js
@@ -129,6 +129,31 @@ window.onclick = function(event) {
 	// $('.time').pickatime();
 
 	
+// Get the modal
+var cartModal = document.getElementById("cartModal");
+
+// Get the button that opens the modal
+var cartBtn = document.getElementById("cart-icon");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+cartBtn.onclick = function() {
+  cartModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  cartModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == cartModal) {
+    cartModal.style.display = "none";
+  }
+}
 
 		
 // // smooth scroll
